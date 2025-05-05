15cb60b3811e0a6bb7860bf0d1ef86bfa17a8808
Carey@254 <caren10koech@gmail.com>
2025-05-05 20:41:19 +0300

    Added new images, updated pages, and improved event scripts

--- script.js
+++ script.js
@@ -308,7 +308,10 @@ hamburger.addEventListener('click', () => {
   }, 4000); // change every 4 seconds
 
 
-
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("active");
+  });
+  
 
 
 
